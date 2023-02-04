a3612046bcc8238d8b8c2a02abc73dc6479eddbe
vladfeigin@microsoft.com <vladfeigin@microsoft.com>
2023-02-04 13:58:07 +0000

    rename notebook

--- day1/3-Create Delta tables. Excersise..sql
+++ day1/3-Create Delta tables. Excersise..sql
@@ -2,9 +2,11 @@
 -- MAGIC %md
 -- MAGIC ### Excersise. Build Bronze Data Lake layer.
 -- MAGIC 
--- MAGIC Create 3 extrenal delta tables: flight_delay_bronze, airport_code_location_bronze, flight_with_weather_bronze.
+-- MAGIC Bronze usually contains the raw data, converted into Delta Lake format. This data is infrequently accessed (cold data). Good for archiving to reduce storage cost.
 -- MAGIC 
--- MAGIC Use temp view with inferSchema = "true"to load original CSV files from the FlightsDelays folder and then use CTAS to create Delta Lake tables in ./bronze folder
+-- MAGIC Create 3 extrenal delta tables: _flight_delay_bronze_, _airport_code_location_bronze_, _flight_with_weather_bronze_.
+-- MAGIC 
+-- MAGIC Use temp view with inferSchema = "true"to load original CSV files from the FlightsDelays folder and then use CTAS to create Delta Lake tables in ./bronze folder.
 -- MAGIC 
 -- MAGIC Every table should be located in the dedicated folder. For example table flight_delay_bronze will be in abfss://${container_name}@${storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightDelay folder
 
@@ -28,7 +30,7 @@
 -- MAGIC %python
 -- MAGIC spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
 -- MAGIC spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
--- MAGIC spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", "")
+-- MAGIC spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", "sp=racwdlmeo&st=2023-02-04T09:29:31Z&se=2023-03-04T17:29:31Z&spr=https&sv=2021-06-08&sr=c&sig=CfujDbdCE2LuJpPEnaq9ooexPK3zN5kf4gbEX8vMlWY%3D")
 
 -- COMMAND ----------
 
@@ -55,7 +57,7 @@ select * from flight_delay_bronze_view
 
 -- COMMAND ----------
 
-select * from flight_delay_bronze
+select * from flight_delay_bronze limit 10
 
 -- COMMAND ----------
 
@@ -64,8 +66,8 @@ describe extended  flight_delay_bronze
 -- COMMAND ----------
 
 -- MAGIC %python 
--- MAGIC display(dbutils.fs.ls(f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightDelays"))%python 
--- MAGIC display(dbutils.fs.ls(f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightDelays"))
+-- MAGIC display(dbutils.fs.ls(f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightDelay"))
+-- MAGIC display(dbutils.fs.ls(f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightDelay"))
 
 -- COMMAND ----------
 
@@ -87,7 +89,7 @@ select * from airport_code_location_bronze_view
 
 -- COMMAND ----------
 
-select * from airport_code_location_bronze
+select * from airport_code_location_bronze limit 10
 
 -- COMMAND ----------
 
@@ -105,6 +107,10 @@ options(
   path = "abfss://${container_name}@${storage_account}.dfs.core.windows.net/FlightsDelays/bronze/FlightWithWeather/"
 )
 as select * from flight_with_weather_bronze_view
+
+-- COMMAND ----------
+
+select * from flight_with_weather_bronze limit 10
 
 -- COMMAND ----------
 
