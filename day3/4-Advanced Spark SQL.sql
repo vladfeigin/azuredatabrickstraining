93656e385b3210e36b9f3c306e510dc9d5e890ac
vladfeigin@microsoft.com <vladfeigin@microsoft.com>
2023-03-25 14:57:28 +0000

    added high order functions

--- day3/4-Advanced Spark SQL.sql
+++ day3/4-Advanced Spark SQL.sql
@@ -22,7 +22,7 @@
 -- MAGIC %python
 -- MAGIC spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
 -- MAGIC spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
--- MAGIC spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", "sp=racwdlmeo&st=2023-02-04T09:29:31Z&se=2023-03-04T17:29:31Z&spr=https&sv=2021-06-08&sr=c&sig=CfujDbdCE2LuJpPEnaq9ooexPK3zN5kf4gbEX8vMlWY%3D")
+-- MAGIC spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", "sp=racwdlmeo&st=2023-03-25T10:44:33Z&se=2023-05-31T18:44:33Z&spr=https&sv=2021-12-02&sr=c&sig=0cKmo1mfJvwXXAaViczNXGSXI60BMsPM5urqr9WBSNQ%3D")
 
 -- COMMAND ----------
 
@@ -39,53 +39,54 @@ options
 
 -- COMMAND ----------
 
-describe raw_kafka_data_view
+select * from raw_kafka_data_view limit 10
 
 -- COMMAND ----------
 
+describe extended raw_kafka_data_view
+
+-- COMMAND ----------
+
+--Cast key and value to string
 create or replace temp view raw_kafka_data_string_view 
 as select string(key), string(value)
 from raw_kafka_data_view 
 
 -- COMMAND ----------
 
-select * from raw_kafka_data_string_view
+select * from raw_kafka_data_string_view limit 10
 
 -- COMMAND ----------
 
-select value:device from raw_kafka_data_string_view
+-- for string type (not structured) we can use : notation to traverse json
+select value:device, value:event_name, value:geo:city from raw_kafka_data_string_view
 
 -- COMMAND ----------
 
-select value:geo:city from raw_kafka_data_string_view
+-- you can parse json into (typed) struct objects (native Spark objects) but for this you need json schema
+-- using schema_of_json and from_json built-in Spark functions
+select value from raw_kafka_data_string_view where value:event_name != "" limit 1
 
 -- COMMAND ----------
 
--- you can parse json into (typed) struct objects but for this you need json schema
-
--- COMMAND ----------
-
--- using schema_of_json and from_json Spark functions
-
-create or replace temp view parsed_kafka_events as
-select from_json(value,schema_of_json('{"device":"Android","ecommerce":{},"event_name":"add_item","event_previous_timestamp":1593880753875794,"event_timestamp":1593880826675403,"geo":{"city":"Chicago","state":"IL"},"items":[{"item_id":"M_STAN_T","item_name":"Standard Twin Mattress","item_revenue_in_usd":595.0,"price_in_usd":595.0,"quantity":1}],"traffic_source":"facebook","user_first_touch_timestamp":1593877772975990,"user_id":"UA000000107369427"}')) as json
+create or replace temp view typed_kafka_events as
+select from_json(value,schema_of_json('{"device":"macOS","ecommerce":{},"event_name":"checkout","event_previous_timestamp":1593880801027797,"event_timestamp":1593880822506642,"geo":{"city":"Traverse City","state":"MI"},"items":[{"item_id":"M_STAN_T","item_name":"Standard Twin Mattress","item_revenue_in_usd":595.0,"price_in_usd":595.0,"quantity":1}],"traffic_source":"google","user_first_touch_timestamp":1593879413256859,"user_id":"UA000000107384208"}')) as event
 from raw_kafka_data_string_view
 
+-- COMMAND ----------
+
+select * from typed_kafka_events limit 10
 
 -- COMMAND ----------
 
-select * from parsed_kafka_events limit 10
+describe typed_kafka_events
 
 -- COMMAND ----------
 
-describe parsed_kafka_events
-
--- COMMAND ----------
-
--- json unpacking. From struct you can flat (unpack) the json
+-- json unpacking. From struct type you can flat (unpack) the json with *
 create or replace temp view kafka_events_flat as
-select json.*
-from parsed_kafka_events
+select event.*
+from typed_kafka_events
 
 -- COMMAND ----------
 
@@ -93,7 +94,7 @@ select * from kafka_events_flat
 
 -- COMMAND ----------
 
--- continue to unpack
+-- continue to unpack with *
 select device,event_name, geo.* from kafka_events_flat
 
 -- COMMAND ----------
@@ -103,11 +104,18 @@ select * from kafka_events_flat where geo.city='Fargo'
 
 -- COMMAND ----------
 
+select items.item_revenue_in_usd
+from kafka_events_flat
+where items.item_revenue_in_usd is not null
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ##### Arrays
 
 -- COMMAND ----------
 
+-- size function
 select max(size(items)) as item_size, device  
 from kafka_events_flat 
 group by device 
@@ -120,13 +128,14 @@ where size(items) > 1
 
 -- COMMAND ----------
 
---explode arrays
+--explode arrays. It will put each element in an array in its own row
 select device, geo.city, explode(items) from  kafka_events_flat
 where size(items) > 1
 
 
 -- COMMAND ----------
 
+-- let's explode and unpack json to the entirely flat table
 create
 or replace temp view transaction_flat as
 select
@@ -148,7 +157,194 @@ from
 
 -- COMMAND ----------
 
+select * from transaction_flat limit 10
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ###### Additonal array functions
+-- MAGIC `array_distinct` removes duplicate elements from array
+-- MAGIC 
+-- MAGIC `flatten` combines multiple arrays into single array
+-- MAGIC 
+-- MAGIC `collect_set` create a set of unique values for a field. Aggregative function.
+
+-- COMMAND ----------
+
+select user_id,
+collect_set(event_name) as event_collection
+from kafka_events_flat
+group by user_id
+
+-- COMMAND ----------
+
+select  collect_set(event_name) as items_list
+from kafka_events_flat
+
+-- COMMAND ----------
+
+select  array_distinct(flatten(collect_set(items))) as items_list
+from kafka_events_flat
+
+-- COMMAND ----------
+
+select user_id,
+collect_set(event_name) as event_collection,
+array_distinct(flatten(collect_set(items.item_id))) as cart_collection
+from kafka_events_flat
+group by user_id
+
+
+
+-- COMMAND ----------
+
+create or replace temp view item_lookup_price_view
+using delta
+options
+(inferSchema = "True", header = "True", path = "abfss://labs-303474@asastoremcw303474.dfs.core.windows.net/FlightsDelays/kafka/item_lookup")
+
+-- COMMAND ----------
+
+select * from item_lookup_price_view
+
+-- COMMAND ----------
+
+create or replace view sales_enriched as 
+select * from (
 select * from transaction_flat 
+)
+
+-- COMMAND ----------
+
+create
+or replace temp view sales_enriched_view as
+select
+  *
+from
+  (
+    select
+      device,
+      event_name,
+      city,
+      state,
+      item_name,
+      item_revenue_in_usd,
+      price_in_usd,
+      quantity,
+      price,
+      a.item_id
+    from
+      transaction_flat a
+      inner join item_lookup_price_view b on a.item_id = b.item_id
+  )
+
+-- COMMAND ----------
+
+drop table sales_enriched;
+
+create table sales_enriched 
+as select * from sales_enriched_view
+
+-- COMMAND ----------
+
+select * from sales_enriched limit 10
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ####Set Operators
+
+-- COMMAND ----------
+
+-- MAGIC %md 
+-- MAGIC Spark supports 
+-- MAGIC 
+-- MAGIC `union`
+-- MAGIC 
+-- MAGIC `minus`
+-- MAGIC 
+-- MAGIC `intersect`
+
+-- COMMAND ----------
+
+-- MAGIC %md 
+-- MAGIC ####Pivot Tables
+-- MAGIC 
+-- MAGIC Turns specific column values into columns.
+
+-- COMMAND ----------
+
+select * from (
+select device,
+      event_name,
+      city,
+      state,
+      item_name,
+      item_revenue_in_usd,
+      price_in_usd,
+      quantity,
+      price,
+      item_id
+      from sales_enriched)
+      pivot ( sum(quantity) FOR item_id in (
+    'P_FOAM_K',
+    'M_STAN_Q',
+    'P_FOAM_S',
+    'M_PREM_Q',
+    'M_STAN_F',
+    'M_STAN_T',
+    'M_PREM_K',
+    'M_PREM_F',
+    'M_STAN_K',
+    'M_PREM_T',
+    'P_DOWN_S',
+    'P_DOWN_K'
+  ));
+      
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ####High Order Functions
+-- MAGIC 
+-- MAGIC `Filter`
+-- MAGIC 
+-- MAGIC `Exists`
+-- MAGIC 
+-- MAGIC `Transform`
+
+-- COMMAND ----------
+
+select * from kafka_events_flat limit 10
+
+-- COMMAND ----------
+
+-- first paramters must be array, i is an iterator 
+
+create or replace temp view standard_iems_events_veiew as
+select user_id, event_name, standard_items from(
+  select user_id, event_name, items,
+  filter (items, i -> i.item_id like '%STAN%') as standard_items
+  from kafka_events_flat
+  )
+where size (standard_items) > 0
+
+-- COMMAND ----------
+
+select * from standard_iems_events_veiew limit 10
+
+-- COMMAND ----------
+
+select
+  user_id,
+  event_name,
+  standard_items,
+  transform (
+    standard_items,
+    i -> cast (i.item_revenue_in_usd * 3.4 as double)
+  ) as nis_price
+from
+  standard_iems_events_veiew
 
 -- COMMAND ----------
 
